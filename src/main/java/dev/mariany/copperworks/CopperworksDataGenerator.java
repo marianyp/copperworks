f3ae3a368c5ed3e4243a5e0a60f2b7a55270a3bf
Mariany P. <marianypinkston@gmail.com>
2024-10-05 16:43:49 -0400

    Initial Commit

---
+++ src/main/java/dev/mariany/copperworks/CopperworksDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.mariany.copperworks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CopperworksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
