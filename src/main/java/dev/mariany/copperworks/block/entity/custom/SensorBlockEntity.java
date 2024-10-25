5cfea05163fcba67cc588d468591407bc5964e9e
Mariany P. <marianypinkston@gmail.com>
2024-10-25 13:44:55 -0400

    Fixed sensor implementation to be blocked by obscuring blocks

--- src/main/java/dev/mariany/copperworks/block/entity/custom/SensorBlockEntity.java
+++ src/main/java/dev/mariany/copperworks/block/entity/custom/SensorBlockEntity.java
@@ -5,14 +5,19 @@ import dev.mariany.copperworks.block.custom.sensor.ChargedSensorBlock;
 import dev.mariany.copperworks.block.entity.ModBlockEntities;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.Entity;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -33,17 +38,44 @@ public class SensorBlockEntity extends BlockEntity {
         Block block = blockState.getBlock();
         Direction facing = ChargedSensorBlock.getDirection(blockState);
 
-        Box visibleArea = Box.enclosing(pos, pos.offset(facing, blockState.get(SENSOR_RANGE)));
+        int range = blockState.get(SENSOR_RANGE);
 
-        List<Entity> visibleEntities = world.getEntitiesByClass(Entity.class, visibleArea,
-                entity -> !entity.isRemoved());
+        Vec3d start = pos.toCenterPos();
 
-        int power = Math.clamp(visibleEntities.size(), 0, 15);
+        for (int i = range; i > 0; i--) {
+            Vec3d end = start.offset(facing, i);
 
-        if (blockState.get(POWER) != power) {
-            world.setBlockState(pos, blockState.with(POWER, power), Block.NOTIFY_ALL);
-            world.updateNeighborsAlways(pos, block);
-            world.updateNeighborsAlways(pos.offset(facing.getOpposite()), block);
+            BlockHitResult raycast = world.raycast(
+                    new RaycastContext(start, end, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE,
+                            ShapeContext.absent()));
+
+            boolean canSee = raycast.getType().equals(HitResult.Type.MISS);
+
+            if (!canSee) {
+                BlockPos hitPos = raycast.getBlockPos();
+                BlockState hitState = world.getBlockState(hitPos);
+                canSee = !hitState.isOpaque() || axisNonSolid(world, hitPos, hitState, facing);
+            }
+
+            if (canSee) {
+                Box visibleArea = Box.enclosing(pos, BlockPos.ofFloored(end));
+                List<Entity> visibleEntities = world.getEntitiesByClass(Entity.class, visibleArea,
+                        entity -> !entity.isRemoved());
+
+                int power = Math.clamp(visibleEntities.size(), 0, 15);
+
+                if (blockState.get(POWER) != power) {
+                    world.setBlockState(pos, blockState.with(POWER, power), Block.NOTIFY_ALL);
+                    world.updateNeighborsAlways(pos, block);
+                    world.updateNeighborsAlways(pos.offset(facing.getOpposite()), block);
+                }
+                return;
+            }
         }
+    }
+
+    private boolean axisNonSolid(World world, BlockPos blockPos, BlockState blockState, Direction direction) {
+        return !(blockState.isSideSolidFullSquare(world, blockPos, direction) || blockState.isSideSolidFullSquare(world,
+                blockPos, direction.getOpposite()));
     }
 }
